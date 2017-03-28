749149441028c274ff6f85edce1c12e29311ba00
chihao <chihaoyo@gmail.com>
2017-03-28 17:56:19 +0800

    Use Vue.js
    Referencing https://github.com/wesley100002001/vuejs-boilerplate

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    generatedAt: new Date(),
+    cover: {
+      title: '沃草給問擂台',
+      styles: {
+          backgroundImage: 'url(asset/key_visual.png)',
+      }
+    },
+    intro: '生如正動實分友時況；愛相氣還算民西毒期先師經運向才管不的後些十？公自本般；自一覺半，的真明義養，我我社白但送改準行品高表也景、理天天廣人性眾十率親想的南。數這東，都為由體當安火，坐中西我在那者不共一小支爸公眼國一參人我！了的出一北失全整求預率眼，輕的？',
+  }
+});
