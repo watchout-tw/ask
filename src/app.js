a36cbe647e40e58b0b8051c8ff501fc38f38124f
chihao <chihaoyo@gmail.com>
2017-03-28 23:17:59 +0800

    Use component; use responsive embeds; add custom styles

--- src/app.js
+++ src/app.js
@@ -1,24 +1,60 @@
+var events = {
+  live: Factory.event(),
+  next: Factory.event(),
+  history: [
+    Factory.event(), Factory.event()
+  ],
+};
+for(var e of events.history) {
+  e.tag = 'li';
+}
+
+Vue.component('event', {
+  template: `
+  <li class="event">
+    <h3>{{ e.title }}</h3>
+    <time :datetime="e.date">{{ e.date }}</time>
+    <time :datetime="e.start">{{ e.start }}</time>
+    <time :datetime="e.end">{{ e.end }}</time>
+    <div class="pgroup">
+      {{ e.description }}
+    </div>
+    <div class="partners">
+      <ul class="list-naked"><label>合作夥伴</label>
+        <li v-for="partner in e.partners">{{ partner.name }}</li>
+      </ul>
+    </div>
+  </li>`,
+  props: {
+    e: {
+      type: Object,
+      validator: function(value) {
+        return true;
+      }
+    }
+  },
+});
+
 var app = new Vue({
   el: '#app',
   data: {
     generatedAt: new Date(),
+    status: 'LIVE',
     cover: {
       title: '沃草給問擂台',
       styles: {
           backgroundImage: 'url(asset/key_visual.png)',
       }
     },
+    live: events.live,
+    next: events.next,
     intro: {
       title: '什麼是給問？',
       content: '生如正動實分友時況；愛相氣還算民西毒期先師經運向才管不的後些十？公自本般；自一覺半，的真明義養，我我社白但送改準行品高表也景、理天天廣人性眾十率親想的南。數這東，都為由體當安火，坐中西我在那者不共一小支爸公眼國一參人我！了的出一北失全整求預率眼，輕的？',
     },
     history: {
       title: '歷史紀錄',
-      content: {
-        events: [
-          Factory.event(), Factory.event()
-        ]
-      }
+      events: events.history,
     }
   }
 });
--- src/data.js
+++ src/data.js
@@ -1,35 +1,63 @@
 var random = function(list) {
   return list && list[Math.floor(Math.random() * list.length)];
 }
+var randomInt = function(from, to) {
+  return Math.floor(from + Math.random()*(to - from + 1));
+}
+var Sample = {
+  target: ['時代力量', '國民黨', '民進黨', '勞動部', '司法改革', '體育改革', '台南市長', '高雄市長'],
+  names: ['蔡英文', '黃國昌', '范雲', '楊智達'],
+  jobTitle: ['總統', '主席', '立法委員', '候選人', '宅宅'],
+  youtubeID: ['4BfxvvbVYWQ', 'THHQzPKuXzI', 'TdtXso7GB_s', 'ADxzZk9wzDk'],
+  partner: ['FGT! 體育改革聯會', '自由時報', 'Google'],
+}
 var Factory = (function Factory() {
-  this.targets = ['時代力量', '國民黨', '民進黨', '勞動部', '司法改革', '體育改革', '台南市長', '高雄市長'];
-  this.names = ['蔡英文', '黃國昌', '范雲', '楊智達'];
-  this.event = function() {
+  this.event = function(options) {
+    // init options
+    options = options || {numberOf: {}};
+    options.numberOf.guests = options.numberOf.guests || randomInt(1, 5);
+    var guests = [];
+    for(var i = 0; i < options.numberOf.guests; i++)
+      guests.push(this.guest());
+
+    options.numberOf.partners = options.numberOf.partners || randomInt(1, 2);
+    var partners = [];
+    for(var i = 0; i < options.numberOf.partners; i++)
+      partners.push(this.partner());
+
+    var youtubeID = random(Sample.youtubeID);
+
+    // make fake data
     return {
-      title: random(this.targets) + '給問嗎？',
+      title: random(Sample.target) + '給問嗎？',
       date: '2015-03-17',
       start: '14:00',
       end: '16:00',
-      description: '',
-      guests: [],
-      partners: [],
+      description: '皮亞價式再應口分，作應自上多言角下生考長頭看年其境父畫，人願加得院外頭改配選車北電小種因目。',
+      guests: guests,
+      partners: partners,
       signup: null,
-      livestream: null,
+      livestream: youtubeID,
       article: null,
       transcript: null,
+      styles: {
+        player: {
+          backgroundImage: 'url(https://' + 'img.youtube.com/vi/' + youtubeID + '/maxresdefault.jpg)'
+        },
+      },
     };
   }
   this.guest = function() {
     return {
-      name: random(this.name),
-      title: '總統',
+      name: random(Sample.name),
+      title: random(Sample.jobTitle),
       photo: 'president.jpg',
       events: [],
     };
   }
   this.partner = function() {
     return {
-      name: 'FGT! 體育改革聯會',
+      name: random(Sample.partner),
       logo: null,
       link: null,
       events: [],
