f1b223679b741457df7326536cad6883441af81e
chihao <chihaoyo@gmail.com>
2017-03-31 10:17:26 +0800

    Use computed styles; restructure data a bit

--- src/app.js
+++ src/app.js
@@ -200,15 +200,19 @@ var app = new Vue({
   el: '#app',
   data: {
     generatedAt: new Date(),
-    status: 'LIVE',
     cover: {
       title: '沃草給問擂台',
-      styles: {
-          backgroundImage: 'url(asset/key_visual.png)',
-      }
+      image: 'key_visual.png',
     },
-    now: events.now,
-    next: events.next,
+    now: {
+      title: 'NOW',
+      status: 'LIVE',
+      event: events.now,
+    },
+    next: {
+      title: 'NEXT',
+      event: events.next,
+    },
     intro: {
       title: '什麼是給問？',
       content: '生如正動實分友時況；愛相氣還算民西毒期先師經運向才管不的後些十？公自本般；自一覺半，的真明義養，我我社白但送改準行品高表也景、理天天廣人性眾十率親想的南。數這東，都為由體當安火，坐中西我在那者不共一小支爸公眼國一參人我！了的出一北失全整求預率眼，輕的？',
@@ -216,6 +220,15 @@ var app = new Vue({
     history: {
       title: '歷史紀錄',
       events: events.history,
+    }
+  },
+  computed: {
+    styles: function() {
+      return {
+        cover: {
+          backgroundImage: 'url(asset/' + this.cover.image + ')'
+        }
+      };
     }
   }
 });
