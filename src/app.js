8899daf73dad0505c13684b3c1c11cf9ca3ad44f
chihao <chihaoyo@gmail.com>
2017-04-05 17:26:20 +0800

    Add opening; use source-han-serif

--- src/app.js
+++ src/app.js
@@ -197,7 +197,7 @@ var app = new Vue({
     generatedAt: new Date(),
     cover: {
       title: '沃草給問擂台',
-      image: 'key_visual.png',
+      image: 'opening.png',
     },
     now: events.now ? {
       title: 'NOW',
@@ -207,7 +207,7 @@ var app = new Vue({
     next: events.next ? {
       title: 'NEXT',
       event: events.next,
-    }: null,
+    } : null,
     intro: {
       title: '什麼是給問？',
       content: ['生如正動實分友時況；愛相氣還算民西毒期先師經運向才管不的後些十？公自本般；自一覺半，的真明義養，我我社白但送改準行品高表也景、理天天廣人性眾十率親想的南。數這東，都為由體當安火，坐中西我在那者不共一小支爸公眼國一參人我！了的出一北失全整求預率眼，輕的？'],
